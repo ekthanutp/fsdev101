4f667973fb097db60d65d8ee8922b8d58b734544
ekthanutp <ekthanut.p@hotmail.com>
2025-04-18 16:44:21 +0700

    learn: React Hook in workshop2

---
+++ workshop2/src/components/Test.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+
+const Test = () => {
+  const [name, setName] = useState("");
+  const [salary, setSalary] = useState(0);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    console.log('Data updated:', data)
+  },[data]);
+
+  const handleData = (e) => {
+        e.preventDefault();
+        if (!name || !salary) {
+        alert("กรุณากรอกให้ครบ");
+        return;
+        }
+
+        const newItem = {
+            id: Date.now(),
+            name,
+            salary,
+          };
+
+        setData([...data,newItem])
+
+        setName("");
+        setSalary(0);
+  };
+
+  return (
+    <>
+      <label>รายการ</label>
+      <input 
+      type="text"
+      value={name}
+      onChange={e => setName(e.target.value)}
+       />
+      <label>ราคา</label>
+      <input type="number" 
+      value={salary}
+      onChange={e => setSalary(Number(e.target.value))}
+      />
+      <button onClick={handleData}>OK</button>
+
+      <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>รายการ</th>
+                <th>ราคา</th>
+            </tr>
+        </thead>
+        <tbody>
+            {data.map((e)=>(
+                <tr key={e.id}>
+                    <td>{e.id}</td>
+                    <td>{e.name}</td>
+                    <td>{e.salary}</td>
+                </tr>
+            ))}
+        </tbody>
+      </table>
+
+      <ul >
+        {data.map((a)=>(<li key={a.id}>ID : {a.id} Name : {a.name} - Salary : {a.salary}</li>))}
+      </ul>
+    </>
+  );
+};
+
+export default Test;
